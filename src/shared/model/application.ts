0586bca395715298ea9115179905ac0cae3c727a
José-Antonio Gonzalez <gonzapep@gmail.com>
2024-03-21 18:55:30 +0100

    Completed the interfaces of the application model

--- src/shared/model/application.ts
+++ src/shared/model/application.ts
@@ -1,56 +1,62 @@
 import { Configuration } from './configuration'
 
-export interface Identifyable {
-  uuid: string
-  lastModified: Date
-  created: Date
-  createdBy: string
+
+/**
+ * A container for the Configuration and the Application
+ */
+export interface CodeHarmony {
+  configuration: Configuration
+  application: Application
 }
 
-export interface Validity {
-  valid: boolean
-  validFrom: Date
-}
-
-export interface Groupable {
-  group: string | undefined
-}
-
-export interface Textfragment extends Identifyable, Groupable {
+/**
+ * The Application contains all the other parts of the model
+ */
+export interface Application extends Identifyable, TextEnabled {
   id: string
-  textVariant: string
-  languageCode: string
-  text: string
+  textElements?: TextElement[]
+  codes?: Code[]
+  intRelations?: IntRelations[]
+  extRelations?: ExtRelations[]
 }
 
-export interface TextEnabled extends Identifyable, Validity, Groupable {
-  textFragments?: Textfragment[]
-}
-
-export interface GlobalElement extends IdleDeadline, TextEnabled, Validity, Groupable {
-  description?: string
-}
-
+/**
+ * A container for for TextFragments
+ */
 export interface TextElement extends GlobalElement {
   id: string
   textConfig: string
 }
 
-export interface Value extends GlobalElement {
-  id: number
-  indentification: string | undefined
+/**
+ * This interface is a Textfragment, which will be used for TextEnabled objects
+ */
+export interface Textfragment extends Identifyable, Groupable {
+  id: string
+  textVariant: string
+  locale: string
+  text: string
 }
 
+/**
+ * A Code with a list of Values
+ */
 export interface Code extends GlobalElement {
   id: string
   values?: Value[] | undefined
 }
 
-export interface IntRelation extends Validity {
-  from: number
-  to: number
+/**
+ * A single value of a Code
+ */
+export interface Value extends GlobalElement {
+  id: number
+  indentification: string | undefined
 }
 
+/**
+ * A relation between two Codes containing IntRelations
+ */
 export interface IntRelations extends GlobalElement {
   id: string
   fromCode: string
@@ -58,12 +64,17 @@ export interface IntRelations extends GlobalElement {
   relations?: IntRelation[]
 }
 
-export interface ExtRelation {
-  code: number
-  externalCode: string | number
-  valid: boolean
+/**
+ * A relation between two Values of a Code
+ */
+export interface IntRelation extends Validity {
+  from: number
+  to: number
 }
 
+/**
+ * A relation between two Codes, one of this Application and one of an external System, containing ExtRelations
+ */
 export interface ExtRelations extends GlobalElement {
   id: string
   idExternal: string
@@ -74,15 +85,50 @@ export interface ExtRelations extends GlobalElement {
   relations?: ExtRelation[]
 }
 
-export interface Application extends Identifyable, TextEnabled {
-  id: string
-  textElements?: TextElement[]
-  codes?: Code[]
-  intRelations?: IntRelations[]
-  extRelations?: ExtRelations[]
+/**
+ * A relation between a Value and an external System Code
+ */
+export interface ExtRelation {
+  code: number
+  externalCode: string | number
+  valid: boolean
 }
 
-export interface CodeHarmony {
-  configuration: Configuration
-  application: Application
+/**
+ * A TextEnabled object can contain Textfragments in different languages and textVariants
+ */
+export interface TextEnabled {
+  textFragments?: Textfragment[]
+}
+
+/**
+ * This interface is used to make a class or object identifiable.
+ */
+export interface Identifyable {
+  uuid: string
+  lastModified: Date
+  created: Date
+  createdBy: string
+}
+
+/**
+ * This interface is used to mark a class or object Valid
+ */
+export interface Validity {
+  valid: boolean
+  validFrom: Date
+}
+
+/**
+ * This interface is used to mark a class or object to be a member of a group
+ */
+export interface Groupable {
+  group: string | undefined
+}
+
+/**
+ * Every main part as TextElement, Code, IntRelations and ExtRelations will be a GlobalElement
+ */
+export interface GlobalElement extends Identifyable, TextEnabled, Validity, Groupable {
+  description?: string
 }
--- src/shared/model/configuration.ts
+++ src/shared/model/configuration.ts
@@ -1,4 +1,16 @@
+/**
+ * The Configuration for an Application
+ */
+export interface Configuration {
+  textVariants: TextVariant[]
+  textConfigs: TextConfig[]
+  groups: Group[]
+  locales: Locale[]
+}
 
+/**
+ * Evenry Application can have multiple TextVariants which describe the different types of TextFragments
+ */
 export interface TextVariant {
   id: string
   mimeType: string
@@ -6,26 +18,31 @@ export interface TextVariant {
   default: boolean
 }
 
+/**
+ * A TextConfig defines types of TextElements
+ */
 export interface TextConfig {
   id: string
   format: string,
   message: string
 }
 
+/**
+ * A Group to mark Implementors of the Interface Groupalbe as members of a group
+ */
 export interface Group {
   id: string
   description: string
 }
 
-export interface Language {
+/**
+ * Defines a locale for an Application
+ * 
+ * “para" id: is the locale, like "de_DE", "en_US" or "de_CH"
+ */
+export interface Locale {
   id: string
   description: string
   defuault: boolean
 }
 
-export interface Configuration {
-  textVariants: TextVariant[]
-  textConfigs: TextConfig[]
-  groups: Group[]
-  languages: Language[]
-}
