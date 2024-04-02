e7ba4b0d7871f2e5cda5968dd4ea211240a3d68e
José-Antonio Gonzalez <gonzapep@gmail.com>
2024-04-02 08:05:16 +0200

    Save current work

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,7 +3,6 @@ import { app, shell, BrowserWindow, ipcMain, Menu, dialog } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import path from 'path'
 import fs from 'fs'
 
 function createWindow(): void {
